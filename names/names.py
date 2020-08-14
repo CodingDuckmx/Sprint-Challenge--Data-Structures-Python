8ca7b0f33b651d41d2582822d1d80533638100c7
Jesus Caballero <57272996+CodingDuckmx@users.noreply.github.com>
2020-08-14 10:22:19 -0500

    Add files via upload

--- names/names.py
+++ names/names.py
@@ -1,210 +1,6 @@
-
-class Node:
-    """
-    Data:
-    Stores two pieces of data:
-    1. The Value
-    2. The Next Node
-
-    Methods/Behavior/Operations:
-    1. Get value
-    2. Set value
-    3. Get next
-    4. Set next
-    """
-    def __init__(self, value=None, next_node=None):
-        # the value at this linked list node
-        self.value = value
-        # reference to the next node in the list
-        self.next_node = next_node
-
-    def get_value(self):
-        return self.value
-
-    def get_next(self):
-        return self.next_node
-
-    def set_next(self, new_next):
-        # set this node's next_node reference to the passed in node
-        self.next_node = new_next
-
-
-class LinkedList:
-    """
-    Data:
-    1. A reference to the head Node
-    2. A reference to the tail Node
-
-    Behavior/Methods:
-    1. Add To Tail
-    2. Prepend (Add a new node and point that Node's next_node at the old Head; update Head pointer)
-    3. Remove Head
-    4. Remove Tail
-    5. Contains?
-    6. Get Maximum?
-    """
-    def __init__(self):
-        # reference to the head of the list
-        self.head = None
-        # reference to the tail of the list
-        self.tail = None
-
-    def add_to_tail(self, value):
-        # wrap the input value in a node
-        new_node = Node(value, None)
-        # check if there is no head (i.e., the list is empty)
-        if not self.head:
-            # if the list is initially empty, set both head and tail to the new node
-            self.head = new_node
-            self.tail = new_node
-        # we have a non-empty list, add the new node to the tail
-        else:
-            # set the current tail's next reference to our new node
-            self.tail.set_next(new_node)
-            # set the list's tail reference to the new node
-            self.tail = new_node
-
-    def remove_head(self):
-        # return None if there is no head (i.e. the list is empty)
-        if not self.head:
-            return None
-        # if head has no next, then we have a single element in our list
-        if not self.head.get_next():
-            # get a reference to the head
-            head = self.head
-            # delete the list's head reference
-            self.head = None
-            # also make sure the tail reference doesn't refer to anything
-            self.tail = None
-            # return the value
-            return head.get_value()
-        # otherwise we have more than one element in our list
-        value = self.head.get_value()
-        # set the head reference to the current head's next node in the list
-        self.head = self.head.get_next()
-        return value
-
-    def remove_tail(self):
-        if not self.head:
-            return None
-        
-        if self.head is self.tail:
-            value = self.head.get_value()
-            self.head = None
-            self.tail = None
-            return value
-        
-        current = self.head
-
-        while current.get_next() is not self.tail:
-            current = current.get_next()
-
-        value = self.tail.get_value()
-        self.tail = current
-        return value
-
-    def contains(self, value):
-        if not self.head:
-            return False
-
-        # Recursive solution
-        # def search(node):
-        #   if node.get_value() == value:
-        #     return True
-        #   if not node.get_next():
-        #     return False
-        #   return search(node.get_next())
-        # return search(self.head)
-    
-        # get a reference to the node we're currently at; update this as we traverse the list
-        current = self.head
-        # check to see if we're at a valid node 
-        while current:
-            # return True if the current value we're looking at matches our target value
-            if current.get_value() == value:
-                return True
-            # update our current node to the current node's next node
-            current = current.get_next()
-        # if we've gotten here, then the target node isn't in our list
-        return False
-
-    def get_max(self):
-        if not self.head:
-            return None
-        # reference to the largest value we've seen so far
-        max_value = self.head.get_value()
-        # reference to our current node as we traverse the list
-        current = self.head.get_next()
-        # check to see if we're still at a valid list node
-        while current:
-            # check to see if the current value is greater than the max_value
-            if current.get_value() > max_value:
-                # if so, update our max_value variable
-                max_value = current.get_value()
-            # update the current node to the next node in the list
-            current = current.get_next()
-        return max_value
-
-
-class BSTNode:
-    def __init__(self, value):
-        self.value = value
-        self.left = None
-        self.right = None
-
-    # Insert the given value into the tree
-    def insert(self, value):
-        if value < self.value:
-            # goes left
-            if not self.left:
-                self.left = BSTNode(value)
-            else:
-                return self.left.insert(value)
-        else:
-            # goes right
-            if not self.right:
-                self.right = BSTNode(value)
-            else:
-                return self.right.insert(value)
-
-
-    # Return True if the tree contains the value
-    # False if it does not
-    def contains(self, target):
-        if target == self.value:
-            return True
-        if target < self.value:
-            if not self.left:
-                return False
-            else:
-                return self.left.contains(target)
-        else:
-            if not self.right:
-                return False
-            else:
-                return self.right.contains(target)
-
-    # Return the maximum value found in the tree
-    def get_max(self):
-        if not self.right:
-            return self.value
-        else:
-            return self.right.get_max()
-
-
-    # Call the function `fn` on the value of each node
-    def for_each(self, fn):
-        fn(self.value)
-        if self.left:
-            self.left.for_each(fn)
-        if self.right:
-            self.right.for_each(fn)
-
-
-##################################################
-
-
 import time
+
+from binary_search_tree import BSTNode
 
 start_time = time.time()
 
@@ -224,45 +20,34 @@ duplicates = []  # Return the list of duplicates in this data structure
 #         if name_1 == name_2:
 #             duplicates.append(name_1)
 
-''' 
-The run time complexity of the old code is O(n^2).
-'''
-name_1 = names_1[0]
-
-tree = BSTNode(name_1)
-
-for name in names_1[1:]:
-    tree.insert(name)
-
-for name in names_2:
-    if tree.contains(name):
-        duplicates.append(name)
-
-'''
-The new run time complexity is O(n ln(n)).
-'''
+#### time complexity O(n)
 
 
+# tree = BSTNode(names_1[0])
 
+# for name in names_1[1:]:
 
-# duplicates = [name for name in names_1 if name in names_2]
+#     tree.insert(name)
 
-''' 
-If we can only use Python arrays, the solution will be quicker but not 
-as quick as using trees.
-'''
+# for name in names_2:
 
+#     if tree.contains(name):
 
-end_time = time.time()
-print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
-print (f"runtime: {end_time - start_time} seconds")
+#         duplicates.append(name)
 
-
-
-
+# time complexity O(n) +  O(log n)  
 
 
 # ---------- Stretch Goal -----------
 # Python has built-in tools that allow for a very efficient approach to this problem
 # What's the best time you can accomplish?  Thare are no restrictions on techniques or data
 # structures, but you may not import any additional libraries that you did not write yourself.
+
+
+duplicates = set(names_1).intersection(set(names_2))
+
+end_time = time.time()
+print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
+print (f"runtime: {end_time - start_time} seconds")
+
+
